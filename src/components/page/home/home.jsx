2d3838eaacf5908450918f6b12d936f895cca23c
Allan Revolorio <alanrevolorio@gmail.com>
2020-01-17 08:07:31 -0600

    Creating base carousel

--- src/components/page/home/home.jsx
+++ src/components/page/home/home.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import CardList from '../../content/card-list/card-list';
+import Carousel from '../../content/carousel/carousel';
 import { Container, Row } from 'reactstrap';
 
 const Home = (props) => (
-    <Container className="mt-5">
-        <Row>
-            <CardList items={props.sections} cardBody="product" lg="6" imageSize="large-image"/>
-        </Row> 
-    </Container>
+    <div>
+        <Carousel/>
+        <Container className="mt-5">
+            <Row>
+                <CardList items={props.sections} cardBody="product" lg="6" imageSize="large-image"/>
+            </Row> 
+        </Container>
+    </div>
 );
 
 export default Home;
